3117fd4d452be92a29a204deb4b6b111e4d1d346
IzkaW <maximax@buziaczek.pl>
2018-01-29 15:52:07 +0100

    Commit init

---
+++ src/main/java/com/jsystems/User.java
@@ -0,0 +1,13 @@
+package com.jsystems;
+
+public class User {
+    public String name ;
+    public String surname ;
+    public int age ;
+    public int wzrost;
+
+    public int countAge(int age){
+        return age+100;
+    }
+
+}
--- src/test/java/jUnitTest/FirstJUnitTest.java
+++ src/test/java/jUnitTest/FirstJUnitTest.java
@@ -1,5 +1,6 @@
 package jUnitTest;
 
+import com.jsystems.User;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -60,7 +61,28 @@ public class FirstJUnitTest extends ConfigJUnit {
         assertThat(integery, hasItems(5));
         assertThat(integery, hasSize(6));
         assertArrayEquals(integery.toArray(), integery2.toArray());
+
     }
 
+    String ala = "ala";
+    String beta2 = "ala";
+    String ala2 = new String("ala");
+    String beta = "ala2";
+
+    @Test
+    public void fourthTest(){
+        assertTrue(ala == beta2);
+       // assertTrue((ala == ala2));
+       // assertTrue(ala.equals(ala2));
+        assertTrue(beta.equals(ala2));
+
+    }
+
+    User naszUser = new User();
+    @Test
+    public void piatyTest(){
+        assertTrue(naszUser.countAge(20) == 120);
+        assertFalse(naszUser.countAge(20) == 90);
+    }
 
 }
