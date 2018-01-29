cfdf00b48424f84cb6a51489e0d0d43589ba4c05
IzkaW <maximax@buziaczek.pl>
2018-01-29 15:18:14 +0100

    Commit init

--- src/test/java/jUnitTest/FirstJUnitTest.java
+++ src/test/java/jUnitTest/FirstJUnitTest.java
@@ -4,9 +4,11 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.endsWith;
-import static org.hamcrest.CoreMatchers.is;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.*;
 
 public class FirstJUnitTest extends ConfigJUnit {
@@ -48,5 +50,17 @@ public class FirstJUnitTest extends ConfigJUnit {
         Integer WartoscTestowa = null;
         assertNull(WartoscTestowa);
     }
+
+    List<Integer> integery = Arrays.asList(1,2,3,4,5,6);
+    List<Integer> integery2 = Arrays.asList(1,2,3,4,5,6);
+
+    @Test
+    public void thirdTest() {
+
+        assertThat(integery, hasItems(5));
+        assertThat(integery, hasSize(6));
+        assertArrayEquals(integery.toArray(), integery2.toArray());
+    }
+
 
 }
