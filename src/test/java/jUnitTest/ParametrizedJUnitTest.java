62ab87ec5556d7698e577f9262ef29b8033423bf
IzkaW <maximax@buziaczek.pl>
2018-01-30 10:58:28 +0100

    Commit init

--- src/test/java/jUnitTest/ParametrizedJUnitTest.java
+++ src/test/java/jUnitTest/ParametrizedJUnitTest.java
@@ -3,6 +3,7 @@ package jUnitTest;
 import com.jsystems.PhoneValidator;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import junitparams.custom.combined.CombinedParameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -13,7 +14,7 @@ import static junit.framework.TestCase.assertTrue;
 @RunWith(JUnitParamsRunner.class)
 public class ParametrizedJUnitTest {
 
-    private PhoneValidator phoneValidator;
+    public PhoneValidator phoneValidator;
 
     @Test
     @Parameters({"1234567890", "123-456-7890", "123.456.7890", "123 456 7890", "123-456-7890 x1234", "123-456-7890 ext1234"})
@@ -36,6 +37,17 @@ public class ParametrizedJUnitTest {
     public void checkIfEmailValid(String email) {
         assertTrue(phoneValidator.isValidEmail(email));
     }
+
+
+    @Test
+    @CombinedParameters({"1,3", "2,4"})
+        public void combinedParam(String first, String second){
+
+            String results = first + second;
+            System.out.println(results);
+            assertTrue(results.contains("1")  || results.contains("3") );
+            assertTrue(results.contains("2")  || results.contains("4") );
+        }
 
 
 }
